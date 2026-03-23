f64306d0302a9dcd7120b7c8539bc172e7d309ed
Gafanhoto-Mestre <243676885+mauro-augusto@users.noreply.github.com>
2026-03-23 00:12:55 -0300

    exercicio ajustado

--- exercicios/ex003/ex003.py
+++ exercicios/ex003/ex003.py
@@ -6,7 +6,7 @@ class ContaBancaria:
     """
     Cria uma conta bancária e permite fazer saques e depósitos!
     """
-    def __init__(self,id,nome,saldo=0):
+    def __init__(self,id='',nome='',saldo=0):
         self.id= id
         self.titular= nome
         self.saldo= saldo
@@ -14,11 +14,9 @@ class ContaBancaria:
     def __str__(self):
         return f'{10*"==="}\n>>>Dados da conta<<<\n ID: {self.id}\n Cliente: {self.titular}\n Saldo Atual: R${self.saldo:,.2f}\n {10*"==="}'
       
-
     def __getstate__(self):
         return f'Dicionário personalizável!'
 
-        
     def depositar(self,valor):
         self.saldo += valor
         return print (f'Depósito de R$ {valor:,.2f} realizado com sucesso!')
@@ -33,38 +31,22 @@ class ContaBancaria:
         tabela.add_column('ID')
         tabela.add_column('Titular')
         tabela.add_column('Saldo')
-
         for v in lista:
             tabela.add_row(*v)
-
         return print(tabela)
-    
-    def Mostrar_Tabela(self):
+
+    def App(self):
+        global lista
         lista= []
         while True:
             identificador= f'{str(randint(500,1000))}'
             titular= str(input('Digite o nome: '))
             saldo= str(input('Digite o Saldo: '))
             cliente= ContaBancaria(identificador, titular, saldo)
-            
             lista.append(cliente.__dict__.values())
             resp= input('Continuar? [s/n] ').upper()
-            if resp != 'S':
+            if resp in 'nN':
                 break
-
-        
-lista= []
-while True:
-    identificador= f'{str(randint(500,1000))}'
-    titular= str(input('Digite o nome: '))
-    saldo= str(input('Digite o Saldo: '))
-    cliente= ContaBancaria(identificador, titular, saldo)
+        cliente.Tabela()
     
-    lista.append(cliente.__dict__.values())
-    resp= input('Continuar? [s/n] ').upper()
-    if resp != 'S':
-        break
-
-cliente.Mostrar_Tabela()
-
-
+ContaBancaria().App()
