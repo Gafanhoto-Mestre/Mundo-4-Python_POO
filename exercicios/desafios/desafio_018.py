0129e7848e2a20ac4bb3d041819aeb3ffc2831c2
Mauro Augusto Januário da Paixão <134173192+Gafanhoto-Mestre@users.noreply.github.com>
2026-04-01 22:32:25 -0300

    resolvendo desafio 18

--- exercicios/desafios/desafio_018.py
+++ exercicios/desafios/desafio_018.py
@@ -2,6 +2,9 @@ from rich import print
 from rich.panel import Panel
 
 class Churrasco:
+    consumo_padrao = 0.400
+    preco_kg = 82.40
+
     def __init__(self, titulo, quant):
         self.titulo = titulo
         self.convidados= quant
@@ -9,10 +12,10 @@ class Churrasco:
     def analisar(self):
         # consumo padrão de carne por pessoa: 400gr
         # Preço da carne R$ 82,40kg
-        total_de_carne= (int(self.convidados) * 0.400)
-        custo_total= float(total_de_carne * 82.40)
+        total_de_carne= (int(self.convidados) * self.consumo_padrao)
+        custo_total= float(total_de_carne * self.preco_kg)
         custo_por_pessoa= custo_total / self.convidados
-        resumo= Panel(f'Analisando [green]{self.titulo}[/] com [blue]{self.convidados} convidados[/]\nCada participante comerá 0.4kg e cada Kg custa R$82.40\nRecomendo [blue]comprar {total_de_carne:.3f}Kg[/] de carne\nO custo total será de [green]R${custo_total:,.2f}[/]\nCada pessoa pagará [yellow]R${custo_por_pessoa:,.2f}[/] para participar.', title=f'{self.titulo}', width=70)
+        resumo= Panel(f'Analisando [green]{self.titulo}[/] com [blue]{self.convidados} convidados[/]\nCada participante comerá {self.consumo_padrao}kg e cada Kg custa R${self.preco_kg:,.2f}\nRecomendo [blue]comprar {total_de_carne:.3f}Kg[/] de carne\nO custo total será de [green]R${custo_total:,.2f}[/]\nCada pessoa pagará [yellow]R${custo_por_pessoa:,.2f}[/] para participar.', title=f'{self.titulo}', width=70)
         return print(resumo)
 
 c1= Churrasco('Churras no Bom ré', 15)
